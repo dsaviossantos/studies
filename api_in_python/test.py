1c331a7f3ecd097a739a80b7bf2450d84e1c8f92
maximilliusgg <dsaviossantos@gmail.com>
2021-10-16 13:18:52 -0300

    api_python progress #1

--- api_in_python/test.py
+++ api_in_python/test.py
@@ -1,6 +1,19 @@
+from flask.globals import request
 import  requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE + "/helloworld/bill")
+data = [{"likes": 2987, "name": "Video's Name 1", "views": 92289},
+        {"likes": 1019, "name": "Video's Name 2", "views": 81192},
+        {"likes": 1987, "name": "Video's Name 3", "views": 9887}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "/video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "/video/0")
+print(response)
+input()
+response = requests.get(BASE + "/video/0")
 print(response.json())
